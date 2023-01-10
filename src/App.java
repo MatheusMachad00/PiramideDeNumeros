ada9f19da69032b6e10ac8471b032f1f764c0e46
MatheusMachad00 <matheus_m_machado@hotmail.com>
2023-01-10 11:59:19 -0300

    First commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        int number;
+
+        System.out.print("Digite um número: ");
+        Scanner in = new Scanner(System.in);
+        number = in.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= i; j++)
+                System.out.print(i);
+            System.out.println("");
+        }
+        in.close();
+    }
+
+}

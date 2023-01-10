705510b2745db7bb07360bc5e88ad482a2f37c04
MatheusMachad00 <matheus_m_machado@hotmail.com>
2023-01-10 12:02:17 -0300

    minor fixes

--- src/App.java
+++ src/App.java
@@ -10,8 +10,9 @@ public class App {
         number = in.nextInt();
 
         for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= i; j++)
+            for (int j = 1; j <= i; j++){
                 System.out.print(i);
+            }
             System.out.println("");
         }
         in.close();
